ba5f0f53771f6f5de784f44c4391e258eb4b83a3
zord <zord@zordan>
2025-02-02 22:23:21 +0300

    added a toggle to enable in cybergrind

--- InstaDeath/InstaDeath.cs
+++ InstaDeath/InstaDeath.cs
@@ -9,7 +9,7 @@ using System;
 
 namespace Instadeath
 {
-    [BepInPlugin("com.zord.instadeath", "InstaDeath", "2.0.0")]
+    [BepInPlugin("com.zord.instadeath", "InstaDeath", "2.0.1")]
     [BepInDependency("com.eternalUnion.pluginConfigurator")]
     public class InstaDeath : BaseUnityPlugin
     {
@@ -22,6 +22,7 @@ namespace Instadeath
 
         private static PluginConfigurator config;
         private static EnumField<RESTARTING_TYPE> restartingType;
+        private static BoolField shouldDoCybergrind;
 
         private void Awake()
         {
@@ -32,14 +33,22 @@ namespace Instadeath
             restartingType = new EnumField<RESTARTING_TYPE>(config.rootPanel, "Restarting Type", "instadeath_restarting_type", RESTARTING_TYPE.Checkpoint);
             restartingType.SetEnumDisplayName(RESTARTING_TYPE.RestartMission, "Restart Mission");
 
+            shouldDoCybergrind = new BoolField(config.rootPanel, "Enable in Cybergrind", "instadeath_is_cybergrind", false);
+
             config.SetIconWithURL("file://" + System.IO.Path.GetDirectoryName(Info.Location) + "\\icon.png");
-            // icon not included in solution, place icon.png in the same folder as mod
+            // icon not included in solution, place icon.png from thunderstore zip file in the same folder as mod
         }
 
         [HarmonyPatch(typeof(NewMovement), "GetHurt")]
         [HarmonyPostfix]
         static void GetHurtPatch()
         {
+            if (shouldDoCybergrind.value == false && GameObject.FindObjectOfType<StatsManager>().endlessMode)
+            {
+                Console.WriteLine("this IS endless mode");
+                return;
+            }
+
             if (NewMovement.Instance.dead && restartingType.value != RESTARTING_TYPE.Disabled)
             {
                 OptionsMenuToManager manager = GameObject.FindObjectOfType<OptionsMenuToManager>();
